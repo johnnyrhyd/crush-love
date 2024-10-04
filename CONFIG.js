8962eb1443e3f037fb7db50b835cc45463e33358
johnnyrhyd <thanhhuy080401@gmail.com>
2024-10-04 14:15:44 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Làm người yêu tớ nhé!",
+  titleHeader: "Hi Em Yêu, anh có điều muốn nói. </br> Anh chin nhỗi em nhiều!",
   descriptionHeader:
-    "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
-  buttonYes: "Tớ đồng ý",
-  buttonNo: "Không đồng ý",
-  titleModar: "Cảm ơn cậu.",
+    "Em tha thứ cho anh nhé. Nếu em im lặng và rời đi thì tức là em ĐỒNG Ý :b.",
+  buttonYes: "2 ly trà sữa thì còn suy nghĩ",
+  buttonNo: "Tội không thể tha",
+  titleModar: "Hehe anh biết em sẽ tha lỗi cho anh mà!.",
   descriptionModar:
-    "Cảm ơn cậu đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cậu. </br> Tớ biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng tớ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br> Tối nay tớ sẽ đến đón cậu. Cậu mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho cậu.</br> Love you <3",
+    "Cảm ơn em đã cho anh thêm một cơ hội để có thể ở bên cạnh, che chở và bảo vệ em thêm lần nữa. </br> Anh biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng anh có một trái tim chân thành. Em chính là cả thế giới của anh. </br> Tối nay anh sẽ đến đón em. Em mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho em.</br> Love you <3",
 };
